31ed81a052ce3804b57e6820d5dac1d9bf3a4b38
Morsed <mdmorsedalam324@gmail.com>
2023-11-05 19:30:35 +0600

    login System Done

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,20 +1,47 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { auth } from "./../Firebase/firebase.config";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 export const MyContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const loginUser=(email,password)=>{
-    return signInWithEmailAndPassword(auth,email,password)
-  }
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubcribe = onAuthStateChanged(auth, (currnUser) => {
+      setUser(currnUser);
+      setLoading(true);
+    });
+    return () => {
+      return unsubcribe();
+    };
+  }, []);
 
   const authData = {
-    createUser,loginUser
+    user,
+    loading,
+    createUser,
+    loginUser,
+    logOut,
   };
   return <MyContext.Provider value={authData}>{children}</MyContext.Provider>;
 };
