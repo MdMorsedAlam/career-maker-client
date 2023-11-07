6b7eb51396d2a02e171538d425c3ea08cbda4cdb
Morsed <mdmorsedalam324@gmail.com>
2023-11-07 14:01:25 +0600

    data deleted from manage page done

--- src/Pages/Services/ManageServices.jsx
+++ src/Pages/Services/ManageServices.jsx
@@ -3,85 +3,89 @@ import MaxWidth from "../../Components/MaxWidth";
 import axios from "axios";
 import { MyContext } from "../../Providers/AuthProvider";
 import Loading from "../../Components/Loading";
+import Swal from "sweetalert2";
 
 const ManageServices = () => {
   const { user } = useContext(MyContext);
-  const [loading,setLoading]=useState(true);
-  const [services,setServices]=useState();
-  
+  const [loading, setLoading] = useState(true);
+  const [services, setServices] = useState();
+
   useEffect(() => {
     axios
       .get(`http://localhost:3737/api/v1/my-services?email=${user?.email}`)
       .then((res) => {
-       setServices(res.data);
-        setLoading(false)
+        setServices(res.data);
+        setLoading(false);
       })
       .catch();
   }, [user]);
-  if(loading){
-   return <Loading/>
+  if (loading) {
+    return <Loading />;
+  }
+
+  const handelDelete=id=>{
+   axios
+   .get(`http://localhost:3737/api/v1/my-services/${id}`)
+   .then((res) => {
+    if(res.data.deletedCount>0){
+     Swal.fire({
+      title: "Good job!",
+      text: "Data Has Been Deleted",
+      icon: "success",
+    });
+    }else{
+     Swal.fire({
+      title: "Opps!!",
+      text: "Something Went Wrong Please Try Again",
+      icon: "error",
+    });
+    }
+     
+   })
+   .catch();
   }
   return (
     <MaxWidth>
       <div>
-
-      <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-    {
-        services?.map(service=><tr key={service._id}>
-         <th>
-           <label>
-             <input type="checkbox" className="checkbox" />
-           </label>
-         </th>
-         <td>
-           <div className="flex items-center space-x-3">
-             <div className="avatar">
-               <div className="mask mask-squircle w-12 h-12">
-                 <img src={service.sphoto} />
-               </div>
-             </div>
-             <div>
-               <div className="font-bold">Hart Hagerty</div>
-               <div className="text-sm opacity-50">United States</div>
-             </div>
-           </div>
-         </td>
-         <td>
-           Zemlak, Daniel and Leannon
-           <br/>
-           <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-         </td>
-         <td>Purple</td>
-         <th>
-           <button className="btn btn-ghost btn-xs">details</button>
-         </th>
-       </tr>)
-       }
-      
-     
-     
-    </tbody>
-   
-    
-  </table>
-</div>
-       
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th className="text-xl font-bold">Photo</th>
+                <th className="text-xl font-bold">Service Name</th>
+                <th className="text-xl font-bold">Price</th>
+                <th className="text-xl font-bold">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {services?.map((service) => (
+                <tr key={service._id}>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="w-44 h-20 rounded-lg">
+                          <img
+                            className="w-full h-full "
+                            src={service.sphoto}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="text-lg font-semibold">{service.sname}</td>
+                  <td className="text-lg font-semibold">{service.price}</td>
+                  <th>
+                    <button onClick={()=>handelDelete(service._id)} className="btn btn-warning mr-2 uppercase">
+                      X
+                    </button>
+                    <button className="btn btn-accent">Update</button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </MaxWidth>
   );
